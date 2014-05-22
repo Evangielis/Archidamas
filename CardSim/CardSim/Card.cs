86d4f396fece71a4d94f989a5ce8a512cdc46d5f
Evangielis <lee.painton@gmail.com>
2014-05-22 10:47:20 -0400

    Started Card project
    Added Card and CardFactory objects

---
+++ CardSim/CardSim/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CardSim
+{
+    /// <summary>
+    /// Represents a Card object which has the following attributes.
+    /// - FTexture : The front face of the card
+    /// - BTexture : The back face of the card 
+    /// </summary>
+    class Card
+    {
+        public Texture2D FTexture { get; private set; }
+        public Texture2D BTexture { get; private set; }
+
+        public Card(Texture2D front, Texture2D back)
+        {
+            this.FTexture = front;
+            this.BTexture = back;
+        }
+    }
+}
